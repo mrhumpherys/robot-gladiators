0eea230329b8ead6fcdd962e4954b5803779e4ba
Michael Humpherys <michaelhumpherys@Michaels-MacBook-Air.local>
2021-01-11 12:59:20 -0500

    shop feature less text

--- assets/js/game.js
+++ assets/js/game.js
@@ -115,19 +115,17 @@ var endGame = function () {
     }
 };
 var shop = function() {
-    var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice.");
+    var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE.");
     // use switch to carry out action
+    parseInt(shopOptionPrompt);
 switch (shopOptionPrompt) {
-    case "REFILL":
-    case "refill":
+    case 1:
      playerInfo.refillHealth();
       break;
-    case "UPGRADE":
-    case "upgrade":
+    case 2:
       playerInfo.upgradeAtack();
       break;
-    case "LEAVE":
-    case "leave":
+    case 3:
       window.alert("Leaving the store.");
   
       // do nothing, so function will end
